6d10345a8332774c91f77f4c48b15cb6deff1da0
Bruno Rodrigues <bruno.rodrigues.gomes@hotmail.com>
2025-04-21 14:43:18 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,73 @@
+import express from "express";
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
+
+const prisma = new PrismaClient()
+
+const app = express();
+app.use(express.json())
+
+app.post('/usuarios', async (req, res) =>{
+
+  await prisma.user.create({
+        data:{
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    res.status(201).json(req.body)
+})
+
+app.get("/usuarios", async (req, res) => {
+
+  const users = await prisma.user.findMany()
+
+
+    res.status(200).json(users)
+});
+
+app.put('/usuarios/:id', async (req, res) =>{
+
+    await prisma.user.update({
+        where:{
+            id:req.params.id
+        },
+
+          data:{
+              email: req.body.email,
+              name: req.body.name,
+              age: req.body.age
+          }
+      })
+  
+      res.status(201).json(req.body)
+  })
+
+  app.delete('/usuarios/:id', async (req, res)=>{
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json({message: 'Usuário deletado com sucesso'})
+  })
+
+
+app.listen(3000)
+
+// As rotas precisam de 2 coisas:
+// 1- o Tipo da Rota (Metodo HTTTP )
+// 2- o endereço da rota
+
+// Meu objetivo: Criar uma API de usuários,
+// -Criar um usuário
+// -Listar todos os usuários
+// -Editar um usuario
+// -Deletar um usuário
+
+
+// usuario: bruno
+// senha: zRzGJvXOYl0kFbZl
